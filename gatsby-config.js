75dc442d90fc8389f849d16227733b61bb7b5ba5
ValeriaVG <valeria.viana.gusmao@gmail.com>
2018-02-18 17:40:16 +0100

    Basic setup

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'ValeriaVG',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi all</h1>
+    <p>Welcome to my blog.</p>
+    <p>That has absolutely nothing.... Yet!</p>
+  </div>
+)
+
+export default IndexPage
